81aa59ccc8da10b89228af96c9f94a1990180836
Daniil Bakach <daniil.bakach@stein-pilz.com>
2022-03-27 11:07:43 +0300

    new build

--- src/header/statistic-header.ts
+++ src/header/statistic-header.ts
@@ -1,6 +1,17 @@
-
+import {bindable} from 'aurelia-framework';
 import './statistic-header.scss';
 
 export class StatisticHeader{
     constructor(){}
+
+    @bindable searchBy: string;
+
+    setRepos(){
+        this.searchBy = 'repos';
+        console.log(this.searchBy);
+    }
+    setUser(){
+        this.searchBy = 'user';
+        console.log(this.searchBy);
+    }
 }
